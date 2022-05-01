7d485168184ee9753f34a3b82036cc72eea4e669
ThiagoSolum <thiago.almeida@solum.agr.br>
2022-05-01 18:07:03 -0300

    finalizando rotas

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Startup Name generator",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
+      theme: ThemeData(
+        // Add the 5 lines from here...
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
         ),
       ),
+      home: const RandomWords(),
     );
   }
 }
